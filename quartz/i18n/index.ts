f20e41f0ac1bffb0a8b6808ffe6030e7ffb9626e
zzzzzllllllaaaa <3559128958@qq.com>
2024-08-21 22:01:13 +0800

    1

--- quartz/i18n/index.ts
+++ quartz/i18n/index.ts
@@ -60,7 +60,7 @@ export const TRANSLATIONS = {
   "pl-PL": pl,
 } as const
 
-export const defaultTranslation = "en-US"
+export const defaultTranslation = "zh-CN"
 export const i18n = (locale: ValidLocale): Translation => TRANSLATIONS[locale ?? defaultTranslation]
 export type ValidLocale = keyof typeof TRANSLATIONS
 export type ValidCallout = keyof CalloutTranslation
